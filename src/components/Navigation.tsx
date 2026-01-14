438686141e95ca51b340faa27a8f90bfa3c4af12
Your Name <you@example.com>
2026-01-14 12:15:05 +0000

    .

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -238,8 +238,8 @@ export const Navigation = React.memo(() => {
         ],
         workers: [
           { title: 'Profile Management', items: [
-            { to: '/skilled-workers/create-profile', label: 'Create Professional Profile', icon: 'fa-user-plus' },
-            { to: '/skilled-workers/upload-samples', label: 'Upload Work Samples', icon: 'fa-upload' }
+            { to: '/skilled-workers/join', label: 'Create Professional Profile', icon: 'fa-user-plus' },
+            { to: '/skilled-workers/upload-samples', label: 'Upload Gig Samples', icon: 'fa-upload' }
           ]}
         ]
       },
